185c805d5501afbf7fe3b8269be91a6cb6f4cf08
saba-calin <sabaila.calin10@gmail.com>
2025-03-17 06:41:26 -0700

    Initial commit

---
+++ studentmanagement/src/main/java/com/mpp/studentmanagement/StudentManagementAppApplication.java
@@ -0,0 +1,21 @@
+package com.mpp.studentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudentManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementAppApplication.class, args);
+	}
+
+	@GetMapping
+	public String fun() {
+		return "hi";
+	}
+
+}
---
+++ studentmanagement/src/test/java/com/mpp/studentmanagement/StudentManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpp.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
