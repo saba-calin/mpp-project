6b5bdb03291c7ec8ba265e7bc9504d53c028749a
saba-calin <sabaila.calin10@gmail.com>
2025-05-14 14:38:49 -0700

    fin

--- student-management-frontend/src/serverUrl.js
+++ student-management-frontend/src/serverUrl.js
@@ -1,2 +1,2 @@
-// export const serverUrl = "http://16.170.203.185:8080";
-export const serverUrl = "http://localhost:8080";
+export const serverUrl = "http://3.84.201.186:8080";
+// export const serverUrl = "http://localhost:8080";
