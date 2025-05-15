273e0a3dadd694bc717a2c23f07e490aa351a8e7
saba-calin <sabaila.calin10@gmail.com>
2025-05-15 09:42:10 -0700

    photos

--- studentmanagement/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p /app/photos
-
-if [ ! -f /app/photos/default-photo.jpg ]; then
-  cp /app/default-photos/default-photo.jpg /app/photos/default-photo.jpg
-fi
-
-exec java -jar app.jar
