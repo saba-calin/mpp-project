5ddb20f3c8c852401cc0ae739971632023001d26
saba-calin <sabaila.calin10@gmail.com>
2025-05-15 13:09:26 -0700

    adding https to backend

--- student-management-frontend/src/serverUrl.js
+++ student-management-frontend/src/serverUrl.js
@@ -1,3 +1,2 @@
-export const serverUrl = "https://3.84.201.186:8443";
-// export const serverUrl = "http://3.84.201.186:8080";
+export const serverUrl = "http://3.84.201.186:8080";
 // export const serverUrl = "http://localhost:8080";
